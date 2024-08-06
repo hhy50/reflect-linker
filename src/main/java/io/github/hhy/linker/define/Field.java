0c09cd6e068b6633757d90a5493ee1bab58cb34d
haiyang <491680922@qq.com>
2024-08-07 00:04:29 +0800

    解析字段表达式

---
+++ src/main/java/io/github/hhy/linker/define/Field.java
@@ -0,0 +1,41 @@
+package io.github.hhy.linker.define;
+
+
+/**
+ * 用来表示目标字段
+ */
+
+public abstract class Field extends TargetPoint {
+
+    /**
+     * 字段名
+     */
+    private String fieldName;
+
+    /**
+     * 上一个字段， 比如 a.b, 那么 this=b, prev=a;
+     */
+    private Field prev;
+
+    public Field(Field prev, String name) {
+        this.prev = prev;
+        this.fieldName = name;
+    }
+
+
+    public Field getPrev() {
+        return prev;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getFullName() {
+        String prefix = "";
+        if (prev != null) {
+            prefix = prev.getFullName() + "_";
+        }
+        return prefix + fieldName;
+    }
+}
