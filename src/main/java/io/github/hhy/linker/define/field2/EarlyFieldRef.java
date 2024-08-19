704790ce935e0c9eb4204914cf5fa01de8bb90e4
haiyang <491680922@qq.com>
2024-08-19 22:26:36 +0800

    1

---
+++ src/main/java/io/github/hhy/linker/define/field2/EarlyFieldRef.java
@@ -0,0 +1,53 @@
+package io.github.hhy.linker.define.field2;
+
+
+import org.objectweb.asm.Type;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/**
+ * 前期就确定好类型的字段，包括：
+ * 1. target字段
+ * 2. 能在上级class中找到的
+ * 3. 使用@Typed注解的字段
+ */
+public class EarlyFieldRef extends FieldRef {
+
+    public Type objDeclaredType;
+
+    public Type objRealType;
+
+    public Type fieldType;
+
+    public boolean isStatic;
+
+    public EarlyFieldRef(FieldRef prev, String objName, Field field) {
+        super(prev, objName, field.getName());
+        this.objDeclaredType = Type.getType(field.getDeclaringClass());
+        this.fieldType = Type.getType(field.getType());
+        this.isStatic = Modifier.isStatic(field.getModifiers());
+//        this.objRealType = Type.getType(field.getType());
+    }
+
+    /**
+     * 仅能表示target
+     * @param prev
+     * @param objName
+     * @param fieldName
+     * @param realType
+     */
+    public EarlyFieldRef(FieldRef prev, String objName, String fieldName, Type realType) {
+        super(prev, objName, fieldName);
+        this.fieldType = realType;
+    }
+
+    public boolean isStatic() {
+        return this.isStatic;
+    }
+
+    @Override
+    public Type getType() {
+        return this.fieldType;
+    }
+}
---
+++ src/main/java/io/github/hhy/linker/define/field2/FieldRef.java
@@ -0,0 +1,52 @@
+package io.github.hhy.linker.define.field2;
+
+
+import io.github.hhy.linker.bytecode.vars.ObjectVar;
+import org.objectweb.asm.Type;
+
+/**
+ * 用来表示目标字段
+ */
+public class FieldRef {
+
+    /**
+     * 对象名
+     */
+    public String objName;
+
+    /**
+     * 字段名
+     */
+    public String fieldName;
+
+    /**
+     * 上一级字段
+     */
+    private FieldRef prev;
+
+    public FieldRef(FieldRef prev, String objName, String name) {
+        this.prev = prev;
+        this.objName = objName;
+        this.fieldName = name;
+    }
+
+    public String getFullName() {
+        String prefix = "";
+        if (prev != null) {
+            prefix = prev.getFullName()+"_$_";
+        }
+        return prefix+fieldName;
+    }
+
+    public String getGetterName() {
+        return getFullName()+"_getter_mh";
+    }
+
+    public String getSetterName() {
+        return getFullName()+"_setter_mh";
+    }
+
+    public Type getType() {
+        return ObjectVar.TYPE;
+    }
+}
