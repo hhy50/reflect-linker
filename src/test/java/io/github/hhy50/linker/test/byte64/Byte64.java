4b5d2e99dbe7b6f6f4ce540f6cdcd1a4961310d9
haiyang <491680922@qq.com>
2024-12-19 22:43:48 +0800

    1.0.9 release

---
+++ src/test/java/io/github/hhy50/linker/test/byte64/Byte64.java
@@ -0,0 +1,8 @@
+package io.github.hhy50.linker.test.byte64;
+
+public interface Byte64 {
+    public double doubleValue();
+    public float floatValue();
+    public long longValue();
+    public int intValue();
+}
---
+++ src/test/java/io/github/hhy50/linker/test/byte64/Byte64Impl.java
@@ -0,0 +1,23 @@
+package io.github.hhy50.linker.test.byte64;
+
+public class Byte64Impl implements Byte64 {
+    @Override
+    public double doubleValue() {
+        return 1.1;
+    }
+
+    @Override
+    public float floatValue() {
+        return 2.2f;
+    }
+
+    @Override
+    public long longValue() {
+        return 3;
+    }
+
+    @Override
+    public int intValue() {
+        return 4;
+    }
+}
