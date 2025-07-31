d96ceae24f9280c2001332966b61af6efdcf4483
hhy50 <491680922@qq.com>
2025-08-01 03:04:14 +0800

    builtin

--- src/main/java/io/github/hhy50/linker/generate/FieldOpsMethodHandler.java
+++ src/main/java/io/github/hhy50/linker/generate/FieldOpsMethodHandler.java
@@ -10,6 +10,7 @@ import io.github.hhy50.linker.generate.bytecode.MethodHandleMember;
 import io.github.hhy50.linker.generate.bytecode.action.Actions;
 import io.github.hhy50.linker.generate.bytecode.action.ChainAction;
 import io.github.hhy50.linker.generate.bytecode.utils.Args;
+import io.github.hhy50.linker.generate.bytecode.vars.VarInst;
 import io.github.hhy50.linker.generate.getter.Getter;
 import org.objectweb.asm.Opcodes;
 
@@ -95,7 +96,7 @@ public abstract class FieldOpsMethodHandler extends MethodHandle {
         classImplBuilder.defineMethod(Opcodes.ACC_PUBLIC, descriptor.getMethodName(), descriptor.getType(), null)
                 .intercept((field.isStatic()
                         ? mhMember.invokeStatic(Args.loadArgs())
-                        : ChainAction.of(getter::invoke).map(varInst -> mhMember.invokeInstance(varInst, Args.loadArgs())))
+                        : ChainAction.of(getter::invoke).then(VarInst::checkNullPointer).map(varInst -> mhMember.invokeInstance(varInst, Args.loadArgs())))
                         .andThen(Actions.areturn(descriptor.getReturnType()))
                 );
     }
